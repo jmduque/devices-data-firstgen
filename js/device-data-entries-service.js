e0892c2a3533a99d0b7f387f4680303aad102e3d
Jose Miguel Duque Vehils <jmduque@outlook.com>
2017-09-12 14:48:25 +0800

    Update device-data-entries-service.js

--- js/device-data-entries-service.js
+++ js/device-data-entries-service.js
@@ -1,8 +1,26 @@
 angular.module('DeviceDataEntries').service('DeviceDataEntriesService', function($http) {
+	this.getParameter = function(paramName) {
+		var searchString = window.location.search.substring(1),
+			i, val, params = searchString.split("&");
+
+		for (i=0;i<params.length;i++) {
+			val = params[i].split("=");
+			if (val[0] == paramName) {
+			return val[1];
+			}
+		}
+		return null;
+	}
+
 	this.getEntries = function() {
+		var finalUrl = "http://ext-devices-api.energolabs.com/api/v1/devices/data?token=6926a45f-7993-4aca-b116-ece074240ba1&limit=1000";
+		var imei = this.getParameter('imei');
+		if (imei) {
+			finalUrl += "&imei=" + imei
+		}
 		return $http({
 			method: 'GET',
-			url: 'http://ext-devices-api.energolabs.com/api/v1/devices/data?token=6926a45f-7993-4aca-b116-ece074240ba1&limit=1000'
+			url: finalUrl
 		});
 	};
 });
